37c1e28b5c42c84232935c8abafb27f1a2b50805
haeil <dlgodlf11@gmail.com>
2022-03-30 15:31:16 +0900

    뭐지이거 뭐한거지....머야

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,24 @@
 "use strict";
 
-console.log("asdfdfsdfwef");
-console.log("by");
+
+const id = document.querySelector("#id");
+const paword = document.querySelector("#psword");
+const loginBtn = document.querySelector("button");
+
+console.log(id);
+
+
+
+loginBtn.addEventListener("click", login);
+
+
+function login() {
+    console.log(id.value);
+
+    const req = {
+        id : id.value,
+        psword : psword.value
+    };
+
+    console.log(req);
+}
