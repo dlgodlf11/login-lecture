a94fd6731a8b92f1e123ae11813829ea2fb5ac97
haeil <dlgodlf11@gmail.com>
2022-03-30 16:52:46 +0900

    add models

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -20,9 +20,9 @@ function login() {
     };
     fetch("/login", {
         method : "POST",
-        // headers : {
-        //     "Content-Type":"application/json",
-        // },
+        headers : {
+            "Content-Type":"application/json",
+        },
         body : JSON.stringify(req)
     })
     .then((res)=>res.json())
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,9 +1,6 @@
 "use strict";
 
-const users = {
-    id : ["dlgodlf11", "govkfl7788", "ghksl36"],
-    psword : ["govkfl11", "govkfl1!", "tngkr2dlrglacor!"]
-};
+const UserStorage = require("../../models/UserStorage");
 
 const output = {
      hello  :  (req, res)=>{
@@ -20,7 +17,7 @@ const process = {
     login:(req, res)=>{
         const id = req.body.id,
         psword = req.body.psword;
-
+        const users = UserStorage.getUsers("id", "psword");
         if(users.id.includes(id)){
             const idx = users.id.indexOf(id);
             if(users.psword[idx] === psword){
